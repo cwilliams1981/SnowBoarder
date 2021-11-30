29aae0f771d45900fa4862862e89074a298bd892
cwilliams1981 <chuckwilliams1981@gmail.com>
2021-11-30 00:55:32 -0500

    Snowboarder 00.00.00.15 - How To Trigger Sound Effects

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -8,12 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] CircleCollider2D HeadCollider;
     [SerializeField] float reloadWait = 1f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider == HeadCollider)
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", reloadWait);
         }
     }
